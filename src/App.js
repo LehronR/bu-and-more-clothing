4f23767c09264fa7120150f67ea7348d0bcd61fa
Lehron Robinson <lehron@Users-MacBook-Pro-5.local>
2019-07-16 19:29:47 -0400

    update app with shop page, collection preview component and collection item component, add shop collections content, add styling for shop page, collection item and collection preview

--- src/App.js
+++ src/App.js
@@ -1,22 +1,16 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import './App.css';
-import Homepage from './components/pages/homepage/homepage.component';
-
-const HatsPage = () => (
-  <div>
-    <h1>HATS PAGE</h1>
-  </div>
-);
+import HomePage from './components/pages/homepage/homepage.component';
+import ShopPage from './components/pages/shop/shop.component';
 
 function App() {
   return (
     <div>
       <Switch>
-        <Route exact path='/' component={Homepage} />
-        <Route path='/hats' component={HatsPage} />
+        <Route exact path='/' component={HomePage} />
+        <Route path='/shop' component={ShopPage} />
       </Switch>
-      {/* <Homepage /> */}
     </div>
   );
 }
